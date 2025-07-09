e287cca62067b38c54fb2a8ba6a93b63db7c439f
joo-devweb <jojorevanaldi@gmail.com>
2025-07-09 22:22:07 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+export default {
+  sessionName: 'CloudBeard-Session',
+  botName: 'CloudBeard Shield',
+  logFilePath: './bug_logs.json', // Path untuk menyimpan log bug
+  performanceLogPath: './performance_metrics.json' // Path untuk log performa
+};
